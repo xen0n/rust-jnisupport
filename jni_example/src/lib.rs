bbea1802612e2185002eadb1902c38d7ddb49083
Wang Xuerui <git@xen0n.name>
2017-04-07 21:16:45 +0800

    init: use good Java class name

--- jni_example/src/lib.rs
+++ jni_example/src/lib.rs
@@ -10,13 +10,13 @@ use jni::objects::JClass;
 use jni::sys::jint;
 
 
-#[jni_export(class = "com.example.test", name = "testMethod1", sig = "(I)I")]
+#[jni_export(class = "com.example.Test", name = "testMethod1", sig = "(I)I")]
 fn test_method_1(_: JNIEnv, _: JClass, x: jint) -> jint {
     x + 1
 }
 
 
-#[jni_export("com.example.test.testMethod2", "(I)I")]
+#[jni_export("com.example.Test.testMethod2", "(I)I")]
 fn test_method_2(_: JNIEnv, _: JClass, x: jint) -> jint {
     x + 2
 }
