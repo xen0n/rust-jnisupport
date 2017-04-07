c1ad6b3f74ee1a9e070c6d2ddb87738a6e7ecc23
Wang Xuerui <git@xen0n.name>
2017-04-07 20:47:08 +0800

    init: wire everything up

--- jni_example/src/lib.rs
+++ jni_example/src/lib.rs
@@ -1,11 +1,16 @@
 #![feature(proc_macro)]
 
+extern crate jni;
 extern crate jni_macros;
 
 use jni_macros::jni_export;
 
+use jni::JNIEnv;
+use jni::objects::JClass;
+use jni::sys::jint;
+
 
 #[jni_export(class = "com.example.test", name = "testMethod", sig = "(I)I")]
-fn test_method(x: i32) -> i32 {
+fn test_method(_: JNIEnv, _: JClass, x: jint) -> jint {
     x + 1
 }
--- jnisupport/src/jni_export.rs
+++ jnisupport/src/jni_export.rs
@@ -16,6 +16,9 @@ pub fn jni_export_impl(args: syn::Attribute, body: syn::Item) -> quote::Tokens {
     println!("DEBUG: args = {:?}", args);
     println!("DEBUG: body = {:?}", body);
 
+    // get rust function name
+    let fn_name: &str = body.ident.as_ref();
+
     // pull params out of args
     let (class, name, sig) = match args.value {
         syn::MetaItem::List(_, params) => {
@@ -58,13 +61,93 @@ pub fn jni_export_impl(args: syn::Attribute, body: syn::Item) -> quote::Tokens {
     println!("DEBUG: class = {}", class);
     println!("DEBUG: name = {}", name);
     println!("DEBUG: sig = {:?}", sig);
+    println!("DEBUG: rust fn name = {}", fn_name);
 
     let sym_name = syn::Ident::new(mangling::get_symbol_name(class, name, &sig));
     println!("DEBUG: symbol name = {:?}", sym_name);
 
-    // TODO
+    // generate signature
+    let fn_name = syn::Ident::new(fn_name);
+    let arg_names: Vec<_> = (0..sig.args().len())
+        .map(|i| syn::Ident::new(format!("arg{}", i)))
+        .collect();
+    let arg_ty: Vec<_> = sig.args()
+        .iter()
+        .map(|ty| jni_ident_from_ty(ty))
+        .collect();
+    let ret_ty = match sig.ret() {
+        Some(ty) => Some(jni_ident_from_ty(ty)),
+        None => None,
+    };
+    println!("DEBUG: args ty = {:?}", arg_ty);
+    println!("DEBUG: ret ty = {:?}", ret_ty);
+
+    // pass through original function
     let mut result = quote::Tokens::new();
     body.to_tokens(&mut result);
 
+    // append export fn to token stream
+    let export_fn = emit_export_fn(fn_name, sym_name, arg_names, arg_ty, ret_ty);
+    export_fn.to_tokens(&mut result);
+
+    println!("DEBUG: result = {:?}", result);
+
     result
+}
+
+
+fn emit_export_fn(name: syn::Ident,
+                  sym_name: syn::Ident,
+                  arg_names: Vec<syn::Ident>,
+                  arg_ty: Vec<syn::Ident>,
+                  ret: Option<syn::Ident>)
+                  -> quote::Tokens {
+    let ret = match ret {
+        Some(ty) => vec![ty],
+        None => vec![],
+    };
+    let arg_names1 = arg_names.clone();
+
+    // TODO: support non-static methods
+    quote!(
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn #sym_name(vm: ::jni::JNIEnv,
+                                    clazz: ::jni::objects::JClass,
+                                    #(#arg_names1: #arg_ty),*
+                                    ) #(-> #ret)* {
+            #name(vm, clazz, #(#arg_names),*)
+        }
+    )
+}
+
+
+fn jni_ident_from_ty(ty: &signature::JavaTy) -> syn::Ident {
+    use signature::JavaTy;
+
+    let ident = match ty {
+        &JavaTy::Boolean => "::jni::sys::jboolean",
+        &JavaTy::Byte => "::jni::sys::jbyte",
+        &JavaTy::Char => "::jni::sys::jchar",
+        &JavaTy::Short => "::jni::sys::jshort",
+        &JavaTy::Int => "::jni::sys::jint",
+        &JavaTy::Long => "::jni::sys::jlong",
+        &JavaTy::Float => "::jni::sys::jfloat",
+        &JavaTy::Double => "::jni::sys::jdouble",
+
+        &JavaTy::Array(_) => {
+            // TODO: differentiate between the distinct names although
+            // identical otherwise
+            "::jni::sys::jarray"
+        }
+
+        &JavaTy::Class(ref s) => {
+            match s.as_str() {
+                "java/lang/String" => "::jni::objects::JString",
+                _ => "::jni::objects::JObject",
+            }
+        }
+    };
+
+    syn::Ident::new(ident)
 }
